25388fd1a68724ee2a07dc54357ab24576a9cefe
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-09-09 20:27:50 +0530

    Add files via upload

---
+++ Numeric.py
@@ -0,0 +1,77 @@
+"""
+Numeric DataTypes:
+
+1. Integer
+2. Float
+3. Complex
+"""
+
+# a = 10
+
+# print("Type: ",type(a))
+# print(a)
+
+# b = 1564984561521321854965132198798754465651115321984456156213479
+
+# print(b)
+# print(type(b))
+
+# c = 12.25
+# print(c)
+# print(type(c))
+
+# d = 125.589798564654213647897895236
+# print(d)
+# print(type(d))
+
+# e = 2 + 5j
+# print(e)
+# print(type(e))
+
+"""
+Addition: 12
+
+12
+
+a = 5
+b = 4
+
+5 + 4 = 9
+5 - 4 = 1
+5 X 4 = 20
+5 / 4 = 1.222
+"""
+# a = 5
+# b = 4
+
+# print(a,"+",b,"=",a+b)
+# print(a,"-",b,"=",a-b)
+# print(a,"X",b,"=",a*b)
+# print(a,"/",b,"=",a/b)
+
+"""
+Arithmetic Operators
+
+1. +
+2. - 
+3. *
+4. /
+5. // (floor division)
+6. ** (Exponent)
+"""
+
+# print(a//b)
+
+# print(a**b)         # 5 ^ 4 = 5 * 5 * 5 * 5 = 625 
+
+print("Enter Number 1: ")
+num1 = int(input())
+print(type(num1))
+print("Num1: ",num1)
+
+num2 = int(input("Enter Number 2: "))
+print("Num2: ",num2)
+
+add = num1 + num2
+
+print("Addition: ",add)
---
+++ second.py
@@ -0,0 +1,79 @@
+# print("How are you?")
+# Single Line comments
+
+"""
+Multi
+Line
+Comments
+"""
+
+'''
+Multi
+Line comments
+'''
+
+# print("How are you?")
+# print('Hey')
+
+
+# end
+
+# print("How are you?",end=" ")
+# print('Hey')
+
+# print("How are you?",end="\t")
+# print('Hey')
+
+# print("How are you?",end="\n")
+# print('Hey')
+
+# print("How are you?",end="Royal")
+# print('Hey')
+
+# print("How are you?",end="")
+# print('Hey')
+
+"""
+Alpha said: "How are you?"
+Alpha said: 'How are you?'
+"""
+
+# print('Alpha said: "How are you"')
+# print("Alpha said: 'How are you'")
+
+"""
+Escaping Sequences/ Characters
+
+\n --> new line
+\t --> tab
+\b --> backspace
+\r --> carriage return
+\
+"""
+
+# print("How are\b you?")
+# print("python is a fun\rJAVA")
+# print("Python is a Programming Language\r1234567")
+
+"""
+Hello\namaste\thank you\bye
+"""
+# print("Hello\ \bnamste\ \bthank you\ \bbye")
+
+# print("Hello\\namaste\\thank you\\bye")
+
+# print('Alpha said:\'Hello\'')
+
+"""
+\t is used to give tab, \\t is used to print \t.
+"""
+# print("\\t is used to give tab, \\\\t is used to print \\t.")
+
+# print("\\\\\\\\t")
+
+"""
+Alpha said Beta:"Gamma is teasing Theta, but Gamma said:'I didn't tease Theta, I tease Delta'"
+"""
+
+# print("Alpha said Beta:\"Gamma is teasing Theta, but Gamma said:'I didn't tease Theta, I tease Delta'\"")
+# print('Alpha said Beta:"Gamma is teasing Theta, but Gamma said:\'I didn\'t tease Theta, I tease Delta\'"')
